aa634b2d396efea43014fdd16690727890005805
LNJplus <lnj@jahnson.de>
2016-01-09 14:56:53 +0100

    fixed bugs, stories can have an end

--- iastories.py
+++ iastories.py
@@ -5,14 +5,14 @@ import xml.etree.ElementTree
 
 NAME = "Interactive Stories CLI"
 AUTHOR = "LNJ"
-VERSION = "0.1.0"
+VERSION = "0.1.1"
 YEAR = "2016"
 
 print( "{} v{} by {}, {}".format(NAME, VERSION, AUTHOR, YEAR) )
 
 def Load():
     # file name (in data folder)
-    FileName = "story_signal_from_mars.de_DE.ias"
+    FileName = "story_signal_from_mars.ias"
     # full file path
     FullPath = os.path.abspath(os.path.join("data", FileName))
     # loaded xml file
@@ -21,42 +21,59 @@ def Load():
     
     print (XMLFile.find("story/title").text, "in", XMLFile.find("story/lang").text)
 
-def Start():
-    if input ("Do you want to start? [Y/n]: ") == "n":
+def Start(again):
+    if again == True:
+        Question = "\nDo you want to start again? [Y/n]: "
+    else:
+        Question = "Do you want to start? [Y/n]: "
+    
+    if input (Question) == "n":
         print ("okay... bye")
         exit()
 
 def DisplayPage(PageNum):
+    StoryName = XMLFile.find("story/title").text
+    
     # the path of the page
     Path = "story/page[@id='{}']/{}"
-    
+
     # the text of the page
     Text = XMLFile.find(Path.format(PageNum, "text")).text
     
     # the number of options
     NumOfOptions = int( XMLFile.find(Path.format(PageNum, "options")).text )
-    
+
     print("––––––––––––––––––––––––––––––––––––––––")
     print(Text)
-    print("\nYour Options: ")
+    print("")
 
-    PrintingOptions = True
-    CurID = int(1)
-    MaxID = NumOfOptions
-    while PrintingOptions:
-        OptionText = XMLFile.find("story/page[@id='{}']/option[@id='{}']".format(PageNum, CurID)).text
-        print (OptionText)
-        if CurID >= MaxID:
-            PrintingOptions = False
-            break
-        else:
-            CurID += 1
-    
-    Answer = AskNextPage(MaxID)
+    # Check if there are no options / this is the end of the story
+    if NumOfOptions == 0:
+        print("THE END!")
+        print("This was the Story: {}".format(StoryName))
+        Start(True)
+    else:
+        print("Your Options: ")
 
-    
-    NextPage = XMLFile.find("story/page[@id='{}']/option[@id='{}']".format(PageNum, Answer)).get("topage")
-    DisplayPage(NextPage)
+        PrintOptions = True
+        CurOpt = int(1)
+        MaxOpt = NumOfOptions
+
+        while PrintOptions:
+            # get and print the text of the option
+            OptionText = XMLFile.find("story/page[@id='{}']/option[@id='{}']".format(PageNum, CurOpt)).text
+            print (OptionText)
+
+            # if this was the last option to print: get the answer and break
+            if CurOpt >= MaxOpt:
+                PrintOptions = False
+                Answer = AskNextPage(MaxOpt)
+                NextPage = XMLFile.find("story/page[@id='{}']/option[@id='{}']".format(PageNum, Answer)).get("topage")
+                DisplayPage(NextPage)
+                break
+            else:
+                CurOpt += 1
+
 
 def AskNextPage(MaxID):
     Answer = input("What do you want to do? [{}-{}]: ".format(1, MaxID))
@@ -68,9 +85,10 @@ def AskNextPage(MaxID):
     if Answer <= MaxID and Answer >= 1:
         return Answer
     else:
+        print("Please enter a number from 1 to {}!".format(MaxID))
         Answer = AskNextPage(MaxID)
         return Answer
 
 Load()
-Start()
+Start(False)
 DisplayPage("1")
